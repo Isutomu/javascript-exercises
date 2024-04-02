e1ac94c14f8ea377c88b729c29e6b32c1cb2d11b
Isutomu <isutomu@gmail.com>
2024-04-02 15:13:22 -0300

    Solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const convertedTemp = ((temperature - 32) * 5 / 9).toFixed(1);
+  const roundedConvertedTemp = Math.round(convertedTemp);
+  return Number(roundedConvertedTemp == convertedTemp ? roundedConvertedTemp : convertedTemp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const convertedTemp = (temperature * 9 / 5 + 32).toFixed(1);
+  const roundedConvertedTemp = Math.round(convertedTemp);
+  return Number(roundedConvertedTemp == convertedTemp ? roundedConvertedTemp : convertedTemp);
 };
 
 // Do not edit below this line
