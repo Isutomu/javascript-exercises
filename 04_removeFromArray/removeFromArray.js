b5144f42e7735683f0c31cff087ba274b5c70a58
Isutomu <isutomu@gmail.com>
2024-04-02 14:20:44 -0300

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...valuesToRemove) {
+  function isInInputArray(value) {
+    for (element of valuesToRemove) {
+        if (value === element) return true;
+    }
+  }
+  return inputArray.filter((arrayElement) => !isInInputArray(arrayElement));
 };
 
 // Do not edit below this line
