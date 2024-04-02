976d791891ddbbb3cc874d7420a2925f8a047c06
Isutomu <isutomu@gmail.com>
2024-04-02 14:06:43 -0300

    Solve exercise 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let reversedWord = [];
+  for (letter of word) {
+    reversedWord.unshift(letter);
+  }
+  return reversedWord.join('');
 };
 
 // Do not edit below this line
