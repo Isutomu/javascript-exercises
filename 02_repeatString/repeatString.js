7be9e4280cbea36c8a2b43a0732157e3991163c4
Isutomu <isutomu@gmail.com>
2024-04-02 13:55:00 -0300

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(word, timesToRepeat) {
+    if (timesToRepeat < 0) {
+        return 'ERROR';
+    }
+    if (word === '') {
+        return '';
+    }
+    
+    let wordRepeated = '';
+    for (let i=0; i<timesToRepeat; i++) {
+        wordRepeated += word;
+    }
+    return wordRepeated;
 };
 
 // Do not edit below this line
