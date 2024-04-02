26b39809151d2c1728d820ae86d0856ba17a2211
Isutomu <isutomu@gmail.com>
2024-04-02 14:36:55 -0300

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min<0 || max<0 || !Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
 
+    // The formula for arithmetic progression is:
+    //(smallerValue + biggerValue) x n / 2
+    //where n is the total amount of numbers being added
+    const sum = (min+max)*(Math.abs(max-min)+1)/2;
+    return sum;
 };
 
 // Do not edit below this line
