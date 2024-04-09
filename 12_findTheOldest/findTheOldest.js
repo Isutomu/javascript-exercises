ca2dc6de2f80e8ae602a23c7fc1023d38c4a6239
Isutomu <isutomu@gmail.com>
2024-04-09 19:46:25 -0300

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, person) => {
+        let age;
+        if(!('yearOfDeath' in person)){
+            age = (new Date()).getFullYear() - person.yearOfBirth;
+        }
+        else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if(age > oldestPerson.age) {
+            oldestPerson.name = person.name;
+            oldestPerson.age = age;
+        }
+        return oldestPerson;
+    },{name: 'lol',age: 0});
 };
 
 // Do not edit below this line
