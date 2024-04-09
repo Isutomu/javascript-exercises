0fbae215a2a0522d8a77c92c2dbb720e832e74ab
Isutomu <isutomu@gmail.com>
2024-04-09 19:06:37 -0300

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    const numberConvert = Number(number);
+    if(numberConvert == 0) {
+        return 0;
+    }
+    if(numberConvert < 0) {
+        return 'OOPS';
+    }
 
+    const fibonacciArr = [1, 1];
+    for(let i=2; i<numberConvert; i++) {
+        fibonacciArr.push(fibonacciArr[i-2] + fibonacciArr[i-1]);
+    }
+    return fibonacciArr[numberConvert-1];
 };
 
 // Do not edit below this line
