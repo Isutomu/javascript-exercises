818af8081b599031775cb32c6b2b1201c53b0e41
Isutomu <isutomu@gmail.com>
2024-04-09 18:51:31 -0300

    Solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let regex = /^[a-zA-Z0-9]+$/;
+    const wordArr = word.split('').filter(letter => regex.test(letter));
+    const reverseWordArr = wordArr.slice().reverse();
+    
+    return wordArr.join('').toLowerCase().localeCompare(reverseWordArr.join('').toLowerCase()) === 0;
 };
 
 // Do not edit below this line
