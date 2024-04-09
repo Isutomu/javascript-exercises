34116b084ba3c1cc7bc47786fa38da1d345daf8c
Isutomu <isutomu@gmail.com>
2024-04-09 18:29:41 -0300

    Solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acum, item) => acum+item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acum, item) => acum*item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let fact = 1;
+  if([0,1].includes(number)) {
+    return 1;
+  }
+  else {
+    for(let i=2; i<=number; i++) {
+      fact *= i;
+    }
+  }
+  return fact;
 };
 
 // Do not edit below this line
